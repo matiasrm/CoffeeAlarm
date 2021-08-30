531460352a8067907bfc46ea01727390559d6fee
Matias Vidoni <matiasrmontenegro@gmail.com>
2021-08-30 11:25:30 -0300

    Initial Commit

---
+++ Shared/AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmApp.swift
+//  Shared
+//
+//  Created by Mati Montenegro on 30/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
